0eb9f1445dcfe1904c54ca8c430201d35bc9c363
crystallu <luyish@indiana.edu>
2018-09-12 01:25:07 -0400

    vue fetch

--- js/dashboard.app.js
+++ js/dashboard.app.js
@@ -12,57 +12,19 @@ var dashboardApp = new Vue({
       "weekly_effort_target": 400,
       tasks:[
       {
-        "id": 1,
-        "title": "Build UI",
-        "type" : "Story",
-        "size" : "M",
-        "team" : "Noble Carrots",
-        "status": "Started",
-        "start_date": "2018-08-02",
-        "close_date": null,
-        "hours_worked":90,
-        "perc_complete": 95,
-        "current_sprint" : true
-      },
-      {
-        "id": 2,
-        "title": "Update unit tests",
-        "type" : "Story",
-        "size" : "L",
-        "team" : "MS Why Us",
-        "status": "Closed",
-        "start_date": "2018-07-15",
-        "close_date": "2018-08-1",
-        "hours_worked": 40,
-        "perc_complete": 100,
-        "current_sprint" : true
-      },
-      {
-        "id": 3,
-        "title": "Write middleware",
-        "type" : "Epic",
-        "size" : "XL",
-        "team" : "California Dream",
-        "status": "Open",
-        "start_date": null,
-        "close_date": null,
-        "hours_worked": 0,
-        "perc_complete": 0,
-        "current_sprint" : true
-      },
-      {
-        "id": 4,
-        "title": "Completion % not saving",
-        "type" : "Bug",
-        "size" : "XS",
-        "team" : "Luke's Parents",
-        "status": "Open",
-        "start_date": "2018-07-29",
-        "close_date": "",
-        "hours_worked": 17,
-        "perc_complete": 80,
-        "current_sprint" : false
+        id: 1,
+        title: " ",
+        type : "",
+        size : "",
+        team : "",
+        status: "",
+        start_date: "2018-08-02",
+        close_date: null,
+        hours_worked:'',
+        perc_complete: '',
+        current_sprint: ''
       }
+
     ]
 },
   computed: {
@@ -80,16 +42,28 @@ var dashboardApp = new Vue({
   },
     pretty_currency: function(val){
       if( val < 1e3){
-        return "$ "+ val
+        return "$ "+ val;
       }
       if (val < 1e6){
-        return "$ " + (val/1e3).toFixed(1) + " k"
+        return "$ " + (val/1e3).toFixed(1) + " k";
       }
-      return "$ " + (val/1e6).toFixed(1) + " M"
+      return "$ " + (val/1e6).toFixed(1) + " M";
     },
     completeClass: function(task){
-      if (task.perc_complete == 100) {return 'alert-success'}
-      if (task.current_sprint && task.hours_worked== 0) {return 'alert-warning'}
+      if (task.perc_complete == 100) {return 'alert-success';}
+      if (task.current_sprint && task.hours_worked== 0) {return 'alert-warning';}
+    },
+    fetchTasks(){
+      fetch("https://raw.githubusercontent.com/tag/iu-msis/dev/public/p1-tasks.json")
+      .then(function (response) {return response.json()}
+      .then(function (json) {dashboardApp.tasks= json })
+      .catch(function(err){
+        console.log('Task fetch error:')
+        console.log(err)
+      })
+    },
+    created(){
+      this.fetchTasks()
     }
 }
 });
